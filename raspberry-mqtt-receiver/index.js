4eea2e7301b26bb1e7c2ac4fef440a2b2e0c49c4
Simon <simmoe@gmail.com>
2021-11-22 10:57:22 +0100

    autoplay eksempel igen

--- raspberry-mqtt-receiver/index.js
+++ raspberry-mqtt-receiver/index.js
@@ -126,11 +126,11 @@ client.subscribe('raspberry/+')
 
   let obj = {
     'content': 'youtube', 
-    'embed': '<iframe id="ytplayer" type="text/html" width="640" height="360"src="https://www.youtube.com/embed/M7lc1UVf-VE?autoplay=1&origin=http://example.com"frameborder="0"></iframe>',
+    'embed': '<iframe width="560" height="315" src="https://www.youtube.com/embed/wUSguNU5YkY?autoplay=1" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>',
     'howlong': 10000
   }
   obj = JSON.stringify(obj)
-//  client.publish('raspberry/hi', obj)
+  client.publish('raspberry/hi', obj)
 
 
 
