c49d7ed1bbdaa456eaacd92fa8fa070ae87be88d
Simon <simmoe@gmail.com>
2022-03-29 10:28:20 +0200

    upload api template

---
+++ nodejs/nodejs-API-template/index.js
@@ -0,0 +1,42 @@
+const express = require('express');
+const fs = require('fs');
+const axios = require('axios');
+const app = express();
+const http = require('http');
+const server = http.createServer(app);
+const { Server } = require("socket.io");
+const io = new Server(server)
+app.use(express.static('public'))
+
+io.on('connection', (socket) => {
+  console.log('a user connected');
+  getActivity()
+});
+
+//api stuff
+const apiKeyLightning = '513232a7-850e-4837-b73e-d1b445baa84d'
+const apiKeyMeteo = '42b0ca74-42a2-476a-9abf-b47f3dd9bfd7'
+
+const getActivity = () => {
+    // GET request for remote image in node.js
+    axios({
+        method: 'get',
+        url: 'https://dmigw.govcloud.dk/v2/metObs/collections/observation/items',
+        //        url: 'https://dmigw.govcloud.dk/v2/lightningdata',
+        responseType: 'json',
+        headers: {
+          'X-Gravitee-Api-Key' : '42b0ca74-42a2-476a-9abf-b47f3dd9bfd7'
+        }
+      })
+        .then(json => {
+            console.log(json.data)
+            //io.emit('activity', json)
+        })
+}
+    
+
+server.listen(3000, () => {
+  console.log('listening on *:3000');
+});
+
+
