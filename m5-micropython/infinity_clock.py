e8288e4d4c5e1320c70a466ab60ed856b30363d1
Simon <simmoe@gmail.com>
2025-03-11 10:19:33 +0100

    chattie magi

--- m5-micropython/infinity_clock.py
+++ m5-micropython/infinity_clock.py
@@ -12,15 +12,13 @@ setScreenColor(0x111111)
 neopixel_1 = unit.get(unit.NEOPIXEL, unit.PORTA, 49)
 
 # Globale variabler
-offset = None
-my_12_hour = None
 apikey = None
-LED_DIV = None
 hue_username = None
-middle_thing = None
 hour_led = None
-minute = None
-sekunder = None
+minute_led = None
+last_minute = -1
+minute_fade_start = None
+fade_duration = 3000  # Fade varighed i ms (3 sekunder)
 
 label0 = M5TextBox(16, 97, "INFINITY", lcd.FONT_DejaVu24, 0xFFFFFF, rotate=0)
 
@@ -35,50 +33,29 @@ def hour_to_led(hour):
     t = hour % 12
     if t == 0:
         t = 12
-    # Udregn offset: (t - 12) * (49/12)
     offset = custom_round((t - 12) * (49 / 12))
-    # Kl. 12 skal svare til LED 24, så:
     led = 24 - offset
-    # Wrap LED til intervallet 1 til 49:
     led = ((led - 1) % 49) + 1
     return led
 
-def compute_fade_color():
-    period = 1000  # Fuld fade-cyklus på 1 sekund
-    t = time.ticks_ms() % period
-    phase = t / period  # Værdi mellem 0.0 og 1.0
-    if phase < 0.5:
-        fraction = phase * 2    # Fade ind: 0 → 1
-    else:
-        fraction = (1 - phase) * 2  # Fade ud: 1 → 0
-    # Definer farver: mørk blå og lyseblå
-    dark_blue = 0x000066
-    light_blue = 0x66ffff
-    r_dark = (dark_blue >> 16) & 0xFF
-    g_dark = (dark_blue >> 8) & 0xFF
-    b_dark = dark_blue & 0xFF
-    r_light = (light_blue >> 16) & 0xFF
-    g_light = (light_blue >> 8) & 0xFF
-    b_light = light_blue & 0xFF
-    # Interpolér farvekomponenterne
-    r = int(r_dark + (r_light - r_dark) * fraction)
-    g = int(g_dark + (g_light - g_dark) * fraction)
-    b = int(b_dark + (b_light - b_dark) * fraction)
-    return (r << 16) | (g << 8) | b
+def minute_to_led(minute):
+    # Mapper 0-59 minutter til LED-positioner med LED 24 for 0 minutter
+    offset = custom_round(minute * (49 / 60))
+    led = 24 - offset
+    led = ((led - 1) % 49) + 1
+    return led
 
 @timerSch.event('ecoTimer')
 def tecoTimer():
-    global hour_led, minute, sekunder, ntp
-    # Beregn time-LED ud fra ntp.hour()
+    global hour_led, minute_led, ntp, last_minute, minute_fade_start
+    current_minute = ntp.minute()
     hour_led = hour_to_led(ntp.hour())
-  
-    # Mapping for minutter og sekunder
-    minute = (28 - (map_value(ntp.minute(), 0, 59, 1, 49))) % 49
-    sekunder = (28 - (map_value(ntp.second(), 0, 59, 1, 49))) % 49
-  
-    # Sæt farver for time og minut (sekund LED opdateres separat)
-    neopixel_1.setColor(hour_led, 0xffffff)   # Timer: gul
-    neopixel_1.setColor(minute, 0x33ff33)       # Minutter: grøn
+    minute_led = minute_to_led(current_minute)
+    # Ved minutændring startes fade-effekten
+    if current_minute != last_minute:
+        minute_fade_start = time.ticks_ms()
+        last_minute = current_minute
+    neopixel_1.setColor(hour_led, 0xffffff)   # Time-LED: hvid
 
 @timerSch.event('ntpTimer')
 def tntpTimer():
@@ -87,18 +64,60 @@ def tntpTimer():
 
 @timerSch.event('fadeTimer')
 def fadeTimer():
-    global sekunder, hour_led, minute
-    # Opdater kun sekund LED'en, hvis den ikke overlapper med time eller minut
-    if sekunder != hour_led and sekunder != minute:
-        neopixel_1.setColor(sekunder, compute_fade_color())
-    # Hvis sekunder overlapper med time/minut, bevares den eksisterende farve
+    global hour_led, minute_led, minute_fade_start, fade_duration
+    period = 60000  # 60 sekunder for sekund-snake
+    ms = time.ticks_ms() % period
+    # Beregn sekund-snake position (bevægelse med uret)
+    pos = 49 - ((ms / period) * 49)
+    snake_length = 5
+    base_color = 0x0000FF  # Kraftig blå til sekund-snake
+
+    for led in range(1, 50):
+        pos_led = led - 1  # 0-indekseret position
+        d = abs(pos_led - pos)
+        if d > 24.5:
+            d = 49 - d
+
+        if led == minute_led:
+            # Minutviseren: varm orange (0xFF4500) med fade-effekt
+            factor = 1.0
+            if minute_fade_start is not None:
+                elapsed = time.ticks_diff(time.ticks_ms(), minute_fade_start)
+                if elapsed < fade_duration:
+                    phase = elapsed / fade_duration
+                    # Fade ud og herefter ind: 1 -> 0 -> 1 over 3 sekunder
+                    if phase < 0.5:
+                        factor = 1.0 - (phase * 2)  # fra 1 til 0
+                    else:
+                        factor = (phase - 0.5) * 2   # fra 0 til 1
+                else:
+                    minute_fade_start = None
+            r = int(0xFF * factor)
+            g = int(0x45 * factor)
+            b = 0
+            minute_color = (r << 16) | (g << 8) | b
+            neopixel_1.setColor(led, minute_color)
+        elif led == hour_led:
+            # Time-LED forbliver hvid (sat af ecoTimer)
+            continue
+        else:
+            # Sekund-snake effekt med lineær fade
+            if d < snake_length:
+                factor = 1.0 - (d / snake_length)
+                r = int(((base_color >> 16) & 0xFF) * factor)
+                g = int(((base_color >> 8) & 0xFF) * factor)
+                b = int((base_color & 0xFF) * factor)
+                color = (r << 16) | (g << 8) | b
+            else:
+                color = 0x000000
+            neopixel_1.setColor(led, color)
 
 apikey = '584E331D'
 hue_username = '6MXQnVOMUBwAuqXnedzRZ4cvhaI9MCLgSjYOrjdx'
 m5mqtt = M5mqtt('', 'mqtt.nextservices.dk', 0, '', '', 300, ssl=True)
 m5mqtt.start()
 m5mqtt.publish(str('KMG CONTROLLER STARTUP'), str('Start'), 0)
-neopixel_1.setBrightness(100)
+neopixel_1.setBrightness(255)  # Ful d. intensitet
 ntp = ntptime.client(host='dk.pool.ntp.org', timezone=1)
 
 timerSch.run('ntpTimer', 360000, 0x00)
